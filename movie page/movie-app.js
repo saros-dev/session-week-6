4f045782ada68c506529077374870f637fdd08cf
Saros <shojaiisaros@yahoo.ca>
2024-09-09 16:45:11 +0330

    unfinal

--- movie page/movie-app.js
+++ movie page/movie-app.js
@@ -10,3 +10,25 @@ else{
     menuList.style.maxHeight = "0px"
 }
 }
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+
+
+
